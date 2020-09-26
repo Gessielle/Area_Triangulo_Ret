3154f36336697a2f5a9df6fe4a01277194dc824b
Gessielle <gessielle-barossi@hotmail.com>
2020-09-26 17:17:21 -0300

    area triangulo ret

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Area_Triangulo_Ret
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Base;
+            double Altura;
+            double Area;
+
+            Console.Write("Digite o valor da base (cm): ");
+            Base = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor da altura (cm): ");
+            Altura = double.Parse(Console.ReadLine());
+            Console.Write("\n");
+
+            Console.WriteLine($"A área do triângulo é: {Area = Base * Altura / 2} (cm²) ");
+
+            Console.WriteLine("Pressione Enter para sair!");
+            Console.ReadKey();
+
+        }
+    }
+}
